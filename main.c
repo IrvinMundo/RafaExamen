cbabe0b5cd3b7c3a9855da38520030b8de7157bc
Hector Flores <frhectorin@gmail.com>
2017-02-13 17:00:22 -0600

    Con struct

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define max_entradas 500
 struct Entrada {
@@ -7,6 +8,19 @@ struct Entrada {
     int lineas[10];   /* el número de línea en el que aparece */
     int total;        /* total de apariciones de la palabra en el texto */
 };
+
+typedef struct Renglon{
+    char* name;
+    int renglon;
+    struct Palabra* head;
+} Renglon;
+
+typedef struct Palabra{
+    char* palabra;
+    int numerofila;
+    int numerocolumna;
+    struct Palabra *siguiente;
+}Palabra;
 
 int split(char *str, char palabras[][21]) {
     int i = -1;
@@ -18,25 +32,46 @@ int split(char *str, char palabras[][21]) {
     }
     return ++i;
 }
-typedef struct Entrada Entrada;
+
+void add_Lista( Palabra ** head,  Palabra * val) {
+    if(*head==NULL){
+        (*head)=val;
+    }else{
+
+        Palabra *current=*head;
+
+        while(current->siguiente!=NULL){
+            current=current->siguiente;
+        }
+
+        current->siguiente=val;
+        val->siguiente=NULL;
+    }
+}
+
+void printWords(char palabras[][21]){
+    for(int i=0;i<10;i++){
+
+    }
+}
+
+
+
+typedef  Entrada Entrada;
 int main() {
     Entrada entradas[max_entradas] = {{"",{0},0}}; /* inicializa el arreglo */
     char *linea;
     size_t tamanio;
+     Palabra *pal =( Palabra*) malloc(sizeof(Palabra));
     while (getline(&linea,&tamanio, stdin) != -1) {
         char palabras[100][21];
         int len = split(linea,palabras);
         int i;
         for (i = 0; i < len; i++)
+
             printf("palabra %d = %s\n",i,palabras[i]);
     }
-}
 
-int main() {
 
-    char* linea[]=
 
-    scanf("%s",)
-
-    return 0;
 }
